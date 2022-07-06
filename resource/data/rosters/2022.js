7a7b795afe2b61c630e89522f2fa96253e80e321
Randy Sim <randysim9@gmail.com>
2022-07-05 22:05:28 -0400

    home page progress, setup for next steps

---
+++ resource/data/rosters/2022.js
@@ -0,0 +1,24 @@
+let TEAMS = {
+    valorant: {
+        captain: "Hidden",
+        members: ["Hidden", "Hidden", "Hidden", "Hidden"],
+        awards: [
+            {
+                title: "EzEsports Qualifier",
+                desc: "Qualifier for EzEsports Playoffs - Valorant"
+            }
+        ],
+        matches: []
+    },
+    league: {
+        captain: "Hidden",
+        members: ["Hidden", "Hidden", "Hidden", "Hidden"],
+        awards: [
+            {
+                title: "EzEsports Winner",
+                desc: "1st Place for EzEsports Playoffs - League of Legends"
+            }
+        ],
+        matches: []
+    }
+};
