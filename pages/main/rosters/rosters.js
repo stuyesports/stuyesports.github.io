6b0d784e33da9e27cab0252fd9d8333112709312
Samuel Chen <schen30@stuy.edu>
2022-10-01 00:06:43 -0400

    mostly done with roster, just need to add actual players

--- pages/main/rosters/rosters.js
+++ pages/main/rosters/rosters.js
@@ -35,20 +35,20 @@ if (game) {
     })
 }
 
-const LoadSelection = () => {
-    let d = TEAMS[selection[1]][selection[0]];
+// const LoadSelection = () => {
+//     let d = TEAMS[selection[1]][selection[0]];
 
-    gameTitle.innerHTML = selection[0][0].toUpperCase() + selection[0].slice(1);
+//     gameTitle.innerHTML = selection[0][0].toUpperCase() + selection[0].slice(1);
 
-    ateam.innerHTML = d.a.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
-    awards.innerHTML = d.a.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
-    if (d.b) {
-        awards.innerHTML += '<div class="break"></div>' + d.b?.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
-        bteam.innerHTML = d.b.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
-    } else {
-        bteam.innerHTML = "None";
-    }
+//     ateam.innerHTML = d.a.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
+//     awards.innerHTML = d.a.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
+//     if (d.b) {
+//         awards.innerHTML += '<div class="break"></div>' + d.b?.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
+//         bteam.innerHTML = d.b.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
+//     } else {
+//         bteam.innerHTML = "None";
+//     }
 
-}
+// }
 
-LoadSelection();
+// LoadSelection();
