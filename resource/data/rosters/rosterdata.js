e2e7b0b942b62a878cbeeedb43faf0dc63a3609d
Randy Sim <randysim9@gmail.com>
2022-10-01 11:24:14 -0400

    updated roster data format

---
+++ resource/data/rosters/rosterdata.js
@@ -0,0 +1,38 @@
+let TEAMS = {
+    "2022": {
+        valorant: {
+            a: {
+                members: ["Hidden - Captain", "Hidden", "Hidden", "Hidden", "Hidden"],
+                subs: ["Hidden"],
+                awards: [
+                    {
+                        title: "EzEsports Qualifier",
+                        desc: "Qualifier for EzEsports Playoffs A - Valorant"
+                    }
+                ],
+                matches: []
+            },
+            b: {
+                members: ["Hidden - Captain", "Hidden", "Hidden", "Hidden", "Hidden"],
+                subs: ["Hidden"],
+                awards: [],
+                matches: []
+            }
+        },
+        league: {
+            a: {
+                members: ["Hidden - Captain", "Hidden", "Hidden", "Hidden", "Hidden"],
+                subs: ["Hidden"],
+                awards: [
+                    {
+                        title: "EzEsports Winner",
+                        desc: "1st Place for EzEsports Playoffs A - League of Legends"
+                    }
+                ],
+                matches: []
+            },
+        }
+    },
+};
+
+export default TEAMS;
