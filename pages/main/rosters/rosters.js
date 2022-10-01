542f1174351873da6579c56d1a0cd379be4149c7
Samuel Chen <schen30@stuy.edu>
2022-10-01 12:26:50 -0400

    linked it to roster.js

--- pages/main/rosters/rosters.js
+++ pages/main/rosters/rosters.js
@@ -1,22 +1,8 @@
-import TEAMS from "../../../resource/data/rosters/2022.js";
+import TEAMS from "../../../resource/data/rosters/rosterdata.js";
 
-let ateam = document.getElementById("ateam");
-let bteam = document.getElementById("bteam");
-let awards = document.getElementById("awards");
-let gameDrop = document.getElementById("gameDrop");
-let yearDrop = document.getElementById("yearDrop");
-let gameTitle = document.getElementById("gameTitle");
+let playerNamesList = document.getElementsByClassName("playerName");
 
 let selection = ["valorant", "2022"];
-
-gameDrop.addEventListener('change', event => {
-    selection[0] = event.target.value;
-    LoadSelection();
-});
-yearDrop.addEventListener('change', event => {
-    selection[1] = event.target.value;
-    LoadSelection();
-})
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -31,20 +17,27 @@ if (game) {
     })
 }
 
-// const LoadSelection = () => {
-//     let d = TEAMS[selection[1]][selection[0]];
+const LoadSelection = () => {
+    let d = TEAMS[selection[1]][selection[0]];
 
-//     gameTitle.innerHTML = selection[0][0].toUpperCase() + selection[0].slice(1);
+    
 
-//     ateam.innerHTML = d.a.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
-//     awards.innerHTML = d.a.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
-//     if (d.b) {
-//         awards.innerHTML += '<div class="break"></div>' + d.b?.awards.map(award => award.title + ": " + award.desc).join('<div class="break"></div>');
-//         bteam.innerHTML = d.b.members.join('<div class="break"></div>') + '<div class="break"></div>Subs: ' + d.a.subs.join(", ");
-//     } else {
-//         bteam.innerHTML = "None";
-//     }
+    let l = 0;
+    for(var i = 0; i < playerNamesList.length; i++){
+        if(l < 5){
+            playerNamesList.item(i).innerHTML = d.a.members[l];
+        }
+        else if(l==5) {
+            playerNamesList.item(i).innerHTML = d.a.subs[0];
+        }
+        else if(l > 5 && l< 11){
+            playerNamesList.item(i).innerHTML = d.b.members[l-6];
+        }
+        else{
+            playerNamesList.item(i).innerHTML = d.b.subs[0];
+        }
+        l++;
+    }
+}
 
-// }
-
-// LoadSelection();
+LoadSelection();
