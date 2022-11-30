f54042331b96a565cc372948a33dac6972555cfd
Randy Sim <randysim9@gmail.com>
2022-11-29 22:59:09 -0500

    home video

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,9 @@ import { Helmet } from "react-helmet";
 
 import styles from "../styles/pages/home.module.css";
 
+import video from "../resource/vids/montage_vala_2022.mp4";
+import logo from "../resource/logos/stuyesportstextonlyblack.png";
+
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -15,7 +18,12 @@ export default class Home extends React.Component {
                     <title>StuyEsports</title>
                 </Helmet>
                 <div className={styles.mainSection}>
-
+                    <div className={styles.mainTitle}>
+                        <img className={styles.mainLogo} src={logo} />
+                    </div>
+                    <video className={styles.mainVideo} loop autoPlay playsinline muted width="1920" height="910">
+                        <source src={video} type="video/mp4" />
+                    </video>
                 </div>
             </div>
         )
